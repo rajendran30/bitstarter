6559d148aabe8eb23c510f2dcbb660454758f97b
Rajendran Rathinasabapathy <rajendran30@gmail.com>
2013-07-19 19:24:59 +0000

    updated for calling html--2

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   //response.send('Hello World 2!');
-    response.send(readFromHTML());
+    response.send('hi there' + readFromHTML());
 });
 
 var port = process.env.PORT || 5000;
