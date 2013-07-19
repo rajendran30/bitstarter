683b5e51737e1470400537b251e658a7e80036e1
Rajendran Rathinasabapathy <rajendran30@gmail.com>
2013-07-19 18:38:33 +0000

    updated for calling html

--- web.js
+++ web.js
@@ -1,12 +1,23 @@
 var express = require('express');
-
+var fs = require ('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('Hello World 2!');
+    respond.send(readFromHTML());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+var readFromHTML = function (){
+fs.readFile('index.html', 'utf8', function (err,data) {
+  if (err) {
+    return err;
+  }
+  return (data);
+});
+
+};
